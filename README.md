c445e71e2ad625c9b3647e7dd5e84d450e8e7394
conrading <trafficking22@gmail.com>
2024-07-02 00:34:34 +0200

    updating css

--- README.md
+++ README.md
@@ -24,8 +24,11 @@ This project is for the promotion of story `Difference is Fine` [IG](https://www
 ##### GitHub Pages
 
 > Builds the app to the `docs` folder.\ [GitHub Pages](https://conrading.github.io/wroclaw-project/) 
+
 > loading pdf from url in component `storyReader.js`, which is constituted from: project url `https://conrading.github.io/wroclaw-project/` + file name `differenceIsFine.pdf` in "Public" folder [link](https://conrading.github.io/wroclaw-project/differenceIsFine.pdf)
+
 > loading img from url, which is constituted from: project url `https://conrading.github.io/wroclaw-project/` + file name `accessFile/01.jpg` in "Public" folder [link](https://conrading.github.io/wroclaw-project/accessFile/01.jpg)
+
 > Failed to load PDF via url [#157](https://github.com/wojtekmaj/react-pdf/issues/157)
 
 
--- src/components/author.js
+++ src/components/author.js
@@ -7,15 +7,21 @@ const Author = () => {
             <div>
                 <img className='author-photo' src='https://pbs.twimg.com/media/FvjIH-eWYAIcGI4?format=jpg&name=small' alt='zdjędzie'/>
             </div>
-            <div>
+            <div className='author-line-container'>
                 <div className='author-line'>
-                    my name is Conrading, I am from Taiwan, currently living in Wrocław, Poland.
+                    <div>my name is Conrading, I am from Taiwan, currently living in Wrocław, Poland. </div>
+                    <div>是暢又澍，來自台灣，現居波蘭 Wrocław。</div>
                 </div>
                 <div className='author-line'>
-                    I am passionate about writing story and music, you can refer to my <a href='https://soundcloud.com/conrading'>Soundcloud</a>. 
+                    <div>I am passionate about writing story and music, you can refer to my <a href='https://soundcloud.com/conrading'>SoundCloud</a>.</div> 
+                    <div>寫作和音樂反映我的靈魂，小說以外，你也可以參考我的音樂在<a href='https://soundcloud.com/conrading'>SoundCloud</a>。</div>
                 </div>
                 <div className='author-line'>
-                    the story "Difference is Fine" now is only available in Chinese, I am growing fanbase in instagram, hope one day I can have the story being translated. 
+                    <div>the story "Difference is Fine" now is only available in Chinese, I am growing fanbase in instagram, hope one day I can have the story being translated. </div>
+                    <div>既然你懂中文，可以看看我的「在所不息」，除此之外，我同時還在IG分享了關於這故事的其他創作，可以從其他方面看量子力學是如何啟發我完成這個故事。</div>
+                </div>
+                <div className='author-line'>
+                    <div>2024 二零二四年</div>
                 </div>
             </div>
         </div>  
