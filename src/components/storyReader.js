ef9f88ef0fb58985c42099e52e52d453bf1230a3
conrading <trafficking22@gmail.com>
2024-07-05 00:03:58 +0200

    updating pdf css

--- src/components/storyReader.js
+++ src/components/storyReader.js
@@ -3,13 +3,15 @@ import React from 'react';
 import './storyReader.css';
 
 const PDFViewer = () => {
+  const pdfWidth = "100%"
+  const pdfHeight = "100%"
   const storySource = "https://conrading.github.io/wroclaw-project/Difference_is_Fine[githubVersion].pdf";
   //const pdfUrl = `${storySource}#toolbar=0`;
   const pdfUrl = `${storySource}`;
 
   return (
     <div className="pdf-viewer-container">
-      <iframe src={pdfUrl} className="pdf-iframe" title="PDF Viewer" />
+      <iframe src={pdfUrl} className="pdf-iframe" title="PDF Viewer" width={pdfWidth} height={pdfHeight}/>
     </div>
   );
 };
