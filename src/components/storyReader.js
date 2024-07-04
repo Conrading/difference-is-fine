d4da06930b3722085ce4e068c6f68a9144fe9598
conrading <trafficking22@gmail.com>
2024-07-04 22:04:57 +0200

    load partial pdf

--- src/components/storyReader.js
+++ src/components/storyReader.js
@@ -3,9 +3,7 @@ import React from 'react';
 import './storyReader.css';
 
 const PDFViewer = () => {
-  //const storySource = "https://conrading.github.io/wroclaw-project/Difference_is_Fine[githubVersion].pdf";
-  const storySource = "https://conrading.github.io/wroclaw-project/differenceIsFine.pdf";
-
+  const storySource = "https://conrading.github.io/wroclaw-project/Difference_is_Fine[githubVersion].pdf";
   const pdfUrl = `${storySource}#toolbar=0`;
 
   return (
