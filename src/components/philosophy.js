dbfb671f3aedcc4cc7fc9da81df5b708d5fbb7ce
conrading <trafficking22@gmail.com>
2024-07-21 15:21:21 +0200

    updating philosophy page

--- src/components/philosophy.js
+++ src/components/philosophy.js
@@ -1,41 +1,79 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './philosophy.css'
 
 const Philosophy = () => {
+    const [isImageEnlarged, setIsImageEnlarged] = useState(false);
+    
+    const handleImageClick = () => {
+        setIsImageEnlarged(true);
+    };
+    
+    const handleCloseClick = () => {
+        setIsImageEnlarged(false);
+    };
+      
     return (
         <body>
             <div className='main-frame'>
-                <img src='https://pbs.twimg.com/media/FvjIH-eWYAIcGI4?format=jpg&name=small' alt='zdjędzie'/>
+                <div className="left-column">
+                    <img 
+                    className="thumbnail" 
+                    src='https://conrading.github.io/wroclaw-project/accessFile/05.jpg' 
+                    alt="Image" 
+                    onClick={handleImageClick} 
+                    />
+                    {isImageEnlarged && (
+                    <div className="overlay" onClick={handleCloseClick}>
+                        <div className="enlarged-image-container">
+                        <img 
+                            src='https://conrading.github.io/wroclaw-project/accessFile/05.jpg' 
+                            alt="enlarged" 
+                            className="enlarged-image" 
+                        />
+                        </div>
+                    </div>
+                    )}
+                </div>
+                <div className='right-column'>
+                    <iframe className='video-centering'
+                        width="392" height="697" 
+                        src="https://www.youtube.com/embed/iYvSiBpKqrY" 
+                        title="how God might Engineer Humans | Difference is Fine" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                    </iframe>
+                </div>
             </div>
-            <div className='video-container'>
-                boy will survive
-                subjective thinking
-                quantum mechanic engineering humans to be different
-                difference is factor of progress
-                machines would never been subjective as we are, they are made for being objective
-                so they need us while we need them
-                <iframe className='video-centering'
-                    width="392" height="697" 
-                    src="https://www.youtube.com/embed/iYvSiBpKqrY" 
-                    title="how God might Engineer Humans | Difference is Fine" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                </iframe>
-                <iframe className='video-centering'
-                    width="392" height="697" 
-                    src="https://www.youtube.com/embed/bo6mf9JpUs0" 
-                    title="Humans might be constructed based on quantum physics to function as highly efficient Machines." 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                </iframe>
-            </div>
-            <div className='opis'>
-            <b>Humans are ultimate version of machine</b>,
-            </div>
-            <div className='opis'>
-            based on this hypothesis, I have finished a story, <b>'Difference is Fine'</b>,
+            <div className='main-frame'>
+                <div className="left-column">
+                    <iframe className='video-centering'
+                        src="https://www.youtube.com/embed/bo6mf9JpUs0" 
+                        title="Humans might be constructed based on quantum physics to function as highly efficient Machines." 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                    </iframe>
+                </div>
+                <div className='right-column'>
+                    <img 
+                    className="thumbnail" 
+                    src='https://conrading.github.io/wroclaw-project/accessFile/06.jpg' 
+                    alt="Image" 
+                    onClick={handleImageClick} 
+                    />
+                    {isImageEnlarged && (
+                    <div className="overlay" onClick={handleCloseClick}>
+                        <div className="enlarged-image-container">
+                        <img 
+                            src='https://conrading.github.io/wroclaw-project/accessFile/06.jpg' 
+                            alt="enlarged" 
+                            className="enlarged-image" 
+                        />
+                        </div>
+                    </div>
+                    )}
+                </div>
             </div>
         </body>
     )
