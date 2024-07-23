7a8e8ecd00be292f17641ed31a98e96fdfac5dcb
conrading <trafficking22@gmail.com>
2024-07-23 22:10:23 +0200

    update css

--- src/components/author.js
+++ src/components/author.js
@@ -11,11 +11,13 @@ const Author = () => {
                 <div className='author-line'>
                     <div>my name is Conrading, I am from Taiwan, currently living in Wrocław, Poland. </div>
                     <div>I am passionate about writing story and music <a href='https://soundcloud.com/conrading'>SoundCloud</a>. Guess I have too much ego, always want to prove myself.</div> 
-                    <div>the story "Difference is Fine" now is only available in Chinese, before I have it translated, maybe you can find different discussion associated with the story on <a href="https://www.instagram.com/differenceisfine/">Instagram</a>, such as quantum mechanics. </div>
+                    <br/>
+                    <div>"Difference is Fine" is my story, now is only available in Chinese, before I have it translated, maybe you can find different discussion associated with the story on <a href="https://www.instagram.com/differenceisfine/">Instagram</a>, such as quantum mechanics. </div>
                     </div>
                 <div className='author-line'>
                     <div>是暢又澍，來自台灣，現居波蘭 Wrocław。</div>
                     <div>寫作和音樂反映了我的靈魂 <a href='https://soundcloud.com/conrading'>SoundCloud</a>，也許也反映了我是多麼自我中心的人。</div>
+                    <br />
                     <div>既然你懂中文，也許可以試閱一點「在所不息」，除此之外，我同時還在<a href="https://www.instagram.com/differenceisfine/">Instagram</a>分享了關於這故事的其他創作，可以從其他方面看量子力學是如何啟發我完成這個故事。</div>
                 </div>
                 <div className='author-line'>
