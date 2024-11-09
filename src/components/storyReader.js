a169773102fa0eb99e52cab802921f302e5ea2f3
conrading <trafficking22@gmail.com>
2024-11-09 10:24:51 +0100

    upload English

--- src/components/storyReader.js
+++ src/components/storyReader.js
@@ -4,24 +4,29 @@ import './storyReader.css';
 
 
 const PDFViewer = () => {
-  const [preReadingToggle, setPreReadingToggle] = useState(true);
+  const [preReadingToggle, setPreReadingToggle] = useState("close");
 
+  //PDFReading
   const pdfWidth = "100%"
   const pdfHeight = "100%"
-  const storySource = "https://conrading.github.io/difference-is-fine/在所不息試閱2024版.pdf";
+  const storyMandarinSource = "https://conrading.github.io/difference-is-fine/在所不息試閱2024版.pdf";
+  const pdfUrlMandarin = `${storyMandarinSource}`;
   //const pdfUrl = `${storySource}#toolbar=0`;
-  const pdfUrl = `${storySource}`;
+  const storyMEnglishSource = "https://conrading.github.io/difference-is-fine/DifferenceIsFine_2024trail.pdf";
+  const pdfUrlEnglish = `${storyMEnglishSource}`;
   const isMobile = window.innerWidth <= 900;
 
-  const switchPreReading = () => {
-    if (preReadingToggle === true) {setPreReadingToggle(false)} else {alert('System Error')}
+  const switchMandarinPreReading = () => {
+    if (preReadingToggle === "close") {setPreReadingToggle("Mandarin")} else {alert('System Error')}
+  }
+  const switchEnglishPreReading = () => {
+    if (preReadingToggle === "close") {setPreReadingToggle("English")} else {alert('System Error')}
   }
   return (
     <div>
-      {preReadingToggle === true && 
+      {preReadingToggle === "close" && 
       <div className='preReadingContainer'>
-        <div className='preReadingText'>sorry, the story currently is only available in Chinese</div>
-        <div className='preReadingText'>English Translation progress: <b>4%</b> in 2024 October</div>
+        <div className='preReadingText'>English Translation is still in progress: <b>24%</b> in 2024 November</div>
         {isMobile ? 
         (
           <div className='preReadingText'>
@@ -29,19 +34,54 @@ const PDFViewer = () => {
             <div>抱歉無法在手機試閱</div>
           </div>
         ): (
-          <div onClick={switchPreReading} className='preReadingToggle'>試閱100頁</div>
+          <div>
+            <div onClick={switchMandarinPreReading} className='preReadingToggle'>試閱100頁</div>
+            <div onClick={switchEnglishPreReading} className='preReadingToggle'>English Trial Version</div>
+          </div>
         )
         }
       </div>}
-      {preReadingToggle === false && 
+      {preReadingToggle === "Mandarin" && 
       <div>
         <div className="pdf-viewer-container">
-          <iframe src={pdfUrl} className="pdf-iframe" title="PDF Viewer" width={pdfWidth} height={pdfHeight}/>
+          <iframe src={pdfUrlMandarin} className="pdf-iframe" title="PDF Viewer" width={pdfWidth} height={pdfHeight}/>
+        </div>
+        <div className='jumpToPudu'>到 <a href='https://www.pubu.com.tw/ebook/451257'>Pubu</a> 閱讀全部 | click <a href='https://www.pubu.com.tw/ebook/451257'>Pubu</a> to read full story</div>
+      </div>}
+      {preReadingToggle === "English" && 
+      <div>
+        <div className="pdf-viewer-container">
+          <iframe src={pdfUrlEnglish} className="pdf-iframe" title="PDF Viewer" width={pdfWidth} height={pdfHeight}/>
         </div>
         <div className='jumpToPudu'>到 <a href='https://www.pubu.com.tw/ebook/451257'>Pubu</a> 閱讀全部 | click <a href='https://www.pubu.com.tw/ebook/451257'>Pubu</a> to read full story</div>
       </div>}
     </div>
   );
 };
+
+/*
+const JSONPageViewer = () => {
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+    fetch('/Mandarin.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setPages(data.page_data);
+      })
+      .catch((error) => console.error('Error loading JSON:', error));
+  }, []);
+
+  return (
+    <div className="json-page-viewer">
+      {pages.map((page, index) => (
+        <div key={index} className="json-page">
+          <img src={page.image_url} alt={`Page ${index + 1}`} width="100%" />
+        </div>
+      ))}
+    </div>
+  );
+};
+*/
 
 export default PDFViewer;
